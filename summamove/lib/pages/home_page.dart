09c44b346b4a76277cf86010ce6953fd4b3d75a3
PS232502 <93653598+PS232502@users.noreply.github.com>
2023-06-14 08:52:33 +0200

    Oefeningen toegevoegd
    Userstory afgerond!

--- summamove/lib/pages/home_page.dart
+++ summamove/lib/pages/home_page.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Image.asset('assets/logo.jpg'),
+        child: Image.asset('lib/assets/logo.jpg'),
       ),
     );
   }
